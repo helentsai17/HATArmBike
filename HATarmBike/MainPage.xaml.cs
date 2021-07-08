43cf8186f8e42a30d7f40b987d411355318184d9
Helen Tsai <helentsaidev@outlook.com>
2021-07-08 15:41:25 -0400

    adding rating and chart line

--- HATarmBike/MainPage.xaml.cs
+++ HATarmBike/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -279,6 +280,7 @@ namespace HATarmBike
             // Do not allow a new Pair operation to start if an existing one is in progress.
             if (isBusy)
             {
+
                 return;
             }
 
@@ -819,7 +821,8 @@ namespace HATarmBike
                 // Configure serial settings
                 serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
                 serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.BaudRate = 115200;
+                serialPort.BaudRate = 14400;
+                //serialPort.BaudRate = 115200;
                 //serialPort.BaudRate = 9600;
                 serialPort.Parity = SerialParity.None;
                 serialPort.StopBits = SerialStopBitCount.One;
@@ -963,6 +966,161 @@ namespace HATarmBike
             }
         }
 
+
+
+        #region buttom click
+
+        int ratingValue;
+        int arrayCount = 1;
+
+        public ObservableCollection<RatingFeeling> LstSource
+        {
+            get { return lstSource; }
+        }
+
+        private ObservableCollection<RatingFeeling> lstSource = new ObservableCollection<RatingFeeling>
+        {
+            new RatingFeeling() { Time = "1-10", Rating = 10 },
+            new RatingFeeling() { Time = "1-10", Rating = 1 },
+        };
+
+        private void Maximal_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 10;
+            //RatingValue.Text = "10";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 10 });
+            arrayCount++;
+        }
+
+        private void Really_Really_hard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 9;
+            //RatingValue.Text = "9";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 9 });
+            arrayCount++;
+        }
+
+        private void Really_hard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 8;
+            //RatingValue.Text = "8";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 8 });
+            arrayCount++;
+        }
+
+        private void Challenging_hard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 7;
+            //RatingValue.Text = "7";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 7 });
+            arrayCount++;
+        }
+
+        private void Hard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 6;
+            //RatingValue.Text = "6";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 6 });
+            arrayCount++;
+        }
+
+        private void Challenging_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 5;
+            //RatingValue.Text = "5";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 5 });
+            arrayCount++;
+        }
+
+        private void Moderate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 4;
+            //RatingValue.Text = "4";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            //lstSource[0].Rating = 4;
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 4 });
+            //ratingArray.Add(ratingValue);
+            arrayCount++;
+        }
+
+        private void Easy_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 3;
+            //RatingValue.Text = "3";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 3 });
+            arrayCount++;
+        }
+
+        private void Really_Easy_Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 2;
+            //RatingValue.Text = "2";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 2 });
+            arrayCount++;
+        }
+
+        private void Rest_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ratingValue = 1;
+            //RatingValue.Text = "1";
+            DateTime localDate = DateTime.Now;
+            var now = localDate.ToString("HH:mm:ss");
+            lstSource.Add(new RatingFeeling() { Time = now, Rating = 1 });
+            arrayCount++;
+        }
+
+        #endregion
+
+
+    }
+
+    public class RatingFeeling : INotifyPropertyChanged
+    {
+        private int _rating;
+        private string _time;
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                this._time = value;
+                NotifyPropertyChanged("time");
+            }
+        }
+        public int Rating
+        {
+
+            get { return _rating; }
+            set
+            {
+                this._rating = value;
+                NotifyPropertyChanged("rating");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
 
     }
 
